530b9c49d478001e6b3a2c40d94621828fa80814
Mulhima <mulhima003@gmail.com>
2023-06-02 22:35:30 +0400

    Add mod code

---
+++ NoDying/NoDying.cs
@@ -0,0 +1,10 @@
+﻿using Modding;
+
+namespace NoDying;
+
+public class NoDying : Mod, ITogglableMod
+{
+    public override void Initialize() => ModHooks.SetPlayerIntHook += DontAllowDying;
+    public void Unload() => ModHooks.SetPlayerIntHook -= DontAllowDying;
+    private int DontAllowDying(string name, int orig) => name == nameof(PlayerData.health) && orig < 1 ? 1 : orig;
+}
