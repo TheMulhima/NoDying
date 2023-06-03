ac0e8ca963e76a4d7c2b1beba68436f2a01b8415
Mulhima <mulhima003@gmail.com>
2023-06-03 13:01:03 +0400

    add version number

--- NoDying/NoDying.cs
+++ NoDying/NoDying.cs
@@ -4,6 +4,7 @@ namespace NoDying;
 
 public class NoDying : Mod, ITogglableMod
 {
+    public override string GetVersion() => "v1.0.0.0";
     public override void Initialize() => ModHooks.SetPlayerIntHook += DontAllowDying;
     public void Unload() => ModHooks.SetPlayerIntHook -= DontAllowDying;
     private int DontAllowDying(string name, int orig) => name == nameof(PlayerData.health) && orig < 1 ? 1 : orig;
